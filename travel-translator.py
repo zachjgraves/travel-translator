105ed740a19e39d8964be90f544d0c4732120275
zachjgraves <127673423+zachjgraves@users.noreply.github.com>
2023-06-14 15:24:27 -0700

    Update travel-translator.py
    prompt

--- travel-translator.py
+++ travel-translator.py
@@ -20,8 +20,8 @@ if language=="Custom":
 if st.button("Submit"):
     prompt="Act as a helpful Enlgish speaking translation assistant\
             Translate the following statement into the {} langauge. \
-            Add two additional options in the input language with English translations. \
-            Add two common responses in the input with English translations. \
+            Add two additional options with translations. \
+            Add two common responses with translations. \
             Add short paragraph in English adding context, to aid in understanding. \
             Statement: {}".format(language, input_phrase)
 
